d3b5824d9e72bf1b5c9327fd7a84941d2840a70b
MurilinhoPs <murilinhorps@gmail.com>
2022-08-25 17:02:47 -0300

    Add layers to collide and timer to destroy on bullet, Delete Weapon to move this logic to every type of player

--- Assets/Scripts/Player/Bullet.cs
+++ Assets/Scripts/Player/Bullet.cs
@@ -1,29 +1,39 @@
+using System.Collections;
 using UnityEngine;
 
 namespace Player
 {
     public class Bullet : MonoBehaviour
     {
-        [SerializeField] private float speed = 20f;
-        [SerializeField] private Rigidbody2D rb;
+        public Rigidbody2D rb;
         [SerializeField] private GameObject impactEffect;
+        [SerializeField] private LayerMask layersToCollide;
+
+        private float autoDestroyTime = 5f;
 
         private void Start()
         {
-            rb.velocity = transform.right * speed;
+            StartCoroutine(DestroySelfAfterSeconds(autoDestroyTime));
         }
 
         private void OnTriggerEnter2D(Collider2D col)
         {
-            if (!col.CompareTag("Boss")) return;
-        
-            // BossHealth enemy = col.GetComponent<BossHealth>();
-            // if (enemy != null)
-            // {
-            //     enemy.TakeDamage(damage);
-            // }
+            if ((layersToCollide.value & (1 << col.gameObject.layer)) > 0)
+            {
+                // BossHealth enemy = col.GetComponent<BossHealth>();
+                // if (enemy != null)
+                // {
+                //     enemy.TakeDamage(damage);
+                // }
 
-            // Instantiate(impactEffect, transform.position, Quaternion.identity);
+                Instantiate(impactEffect, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
+        }
+
+        private IEnumerator DestroySelfAfterSeconds(float destroyTime)
+        {
+            yield return new WaitForSeconds(destroyTime);
             Destroy(gameObject);
         }
     }
