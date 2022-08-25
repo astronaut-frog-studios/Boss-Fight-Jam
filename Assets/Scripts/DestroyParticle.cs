6735a795dc52ba1e36cbe6542000b535ab15cd87
MurilinhoPs <murilinhorps@gmail.com>
2022-08-25 17:01:43 -0300

    Create DestroyParticle to add on Bullet

---
+++ Assets/Scripts/DestroyParticle.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using UnityEngine;
+
+public class DestroyParticle : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem particle;
+    
+    private void Start()
+    {
+        StartCoroutine(DestroyAfterDuration());
+    }
+
+    private IEnumerator DestroyAfterDuration()
+    {
+        yield return new WaitForSeconds(particle.main.duration);
+        Destroy(gameObject);
+    }
+}
